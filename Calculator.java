8e03805f3e5e757130ef27cb75d59bdfe5b04f97
Dyabamix <48042387+Dyabamix@users.noreply.github.com>
2021-01-29 11:18:55 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -9,31 +9,31 @@ public class Calculator {
         String input = scanner.nextLine();
         String[] arrayInput = input.split(" ");
         int a;
-        boolean aIsRome;
+        boolean aIsRoman;
         try {
             a = Integer.parseInt(arrayInput[0]);
-            aIsRome = false;
+            aIsRoman = false;
         } catch (NumberFormatException e) {
-            aIsRome = true;
+            aIsRoman = true;
             a = ArabicToRomanAndBack.romanToArabic(arrayInput[0]);
         }
 
         int b;
-        boolean bIsRome;
+        boolean bIsRoman;
         try {
             b = Integer.parseInt(arrayInput[2]);
-            bIsRome = false;
+            bIsRoman = false;
         } catch (NumberFormatException e) {
-            bIsRome = true;
+            bIsRoman = true;
             b = ArabicToRomanAndBack.romanToArabic(arrayInput[2]);
         }
 
-        if (a > 10 || b > 10){
+        if ((a > 10 || b > 10) && (a < 1 || b < 1) ) {
             throw new RuntimeException("Числа превышают необходимый диапазон");
         }
-        if (aIsRome == bIsRome) {
+        if (aIsRoman == bIsRoman) {
             int result = Calculate.calculate(a, b, arrayInput[1]);
-            if (aIsRome) {
+            if (aIsRoman) {
                 System.out.println(ArabicToRomanAndBack.arabicToRoman(result));
             } else {
                 System.out.println(result);
