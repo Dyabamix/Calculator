3f6db4c24052c409ffa0f2ad69cc96e62541b6d4
Dyabamix <48042387+Dyabamix@users.noreply.github.com>
2021-01-29 11:21:40 +0300

    Update ArabicToRomanAndBack.java

--- ArabicToRomanAndBack.java
+++ ArabicToRomanAndBack.java
@@ -1,9 +1,9 @@
 public class ArabicToRomanAndBack {
 
-    public static final String[] d0 = new String[]{
+    public static final String[] romanUnits = new String[]{
             "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"
     };
-    public static final String[] d1 = new String[]{
+    public static final String[] romanTens = new String[]{
             "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"
     };
 
@@ -12,21 +12,22 @@ public class ArabicToRomanAndBack {
         String firstPart = "";
         String secondPart = "";
         if (number / 10 - 1 >= 0) {
-            firstPart = d1[number / 10 - 1];
+            firstPart = romanTens[number / 10 - 1];
         }
         if (number % 10 - 1 >= 0) {
-            secondPart = d0[number % 10 - 1];
+            secondPart = romanUnits[number % 10 - 1];
         }
         return firstPart + secondPart;
     }
 
-    public static int romanToArabic(String s) {
-        for (int i = 0; i < d0.length; i++) {
-            if (d0[i].equals(s)) {
+    public static int romanToArabic(String number) {
+        for (int i = 0; i < romanUnits.length; i++) {
+            if (romanUnits[i].equals(number)) {
                 return i + 1;
             }
         }
-            if (d1[0].equals(s)) {
+
+        if (romanTens[0].equals(number)) {
             return 10;
         }
 
